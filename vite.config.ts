0bd00b98e5a8973a1880fa8a083adf23afe05c40
replica3d <david@replica3d.pl>
2025-02-04 12:31:19 +0100

    Config fix

--- vite.config.ts
+++ vite.config.ts
@@ -24,61 +24,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: 'index.html',
-      output: {
-        // Obfuscate file names
-        entryFileNames: 'assets/[hash].js',
-        chunkFileNames: 'assets/[hash].js',
-        assetFileNames: 'assets/[hash].[ext]',
-        // Disable code splitting to make reverse engineering harder
-        manualChunks: undefined
-      }
+      input: 'index.html'
     },
-    // Enable maximum minification
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-        pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.trace'],
-        passes: 3,
-        dead_code: true,
-        keep_fargs: false,
-        toplevel: true,
-        unsafe: true,
-        unsafe_math: true,
-        unsafe_methods: true,
-        unsafe_proto: true,
-        unsafe_regexp: true,
-        unsafe_undefined: true
-      },
-      mangle: {
-        toplevel: true,
-        safari10: true,
-        properties: {
-          regex: /^_/,
-          reserved: ['__VITE_', '__STACKBLITZ_', '__REACT_']
-        }
-      },
-      format: {
-        comments: false
-      },
-      ecma: 2020,
-      module: true
-    },
-    sourcemap: false,
-    // Additional build options for protection
-    cssMinify: true,
-    assetsInlineLimit: 0,
-    modulePreload: false
+    sourcemap: true
   },
   resolve: {
     alias: {
       '@': '/src'
     }
-  },
-  // Disable source maps in development
-  css: {
-    devSourcemap: false
   }
 });
